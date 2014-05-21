eaaec09e9a4c842c12101b663d03ffb45f594a34
Clement BOULY <bouly.clement@gmail.com>
2014-05-21 12:36:36 +0200

    Keyboard fragment update

--- src/main/java/com/baloomba/feeligo/StickerKeyboard.java
+++ src/main/java/com/baloomba/feeligo/StickerKeyboard.java
@@ -1,11 +1,17 @@
 package com.baloomba.feeligo;
 
+import android.os.Bundle;
 import android.support.v4.app.Fragment;
-
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 
 public class StickerKeyboard extends Fragment {
 
     // <editor-fold desc="VARIABLES">
+
+    private static final String TAG = StickerKeyboard.class.getSimpleName();
+    private View mView;
 
     // </editor-fold>
 
@@ -14,6 +20,21 @@ public class StickerKeyboard extends Fragment {
     // </editor-fold>
 
     // <editor-fold desc="FRAGMENT OVERRIDDEN METHODS">
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        mView = inflater.inflate(R.layout.fragment_keyboard, container, false);
+        if (mView != null) {
+
+        }
+        return mView;
+    }
 
     // </editor-fold>
 
